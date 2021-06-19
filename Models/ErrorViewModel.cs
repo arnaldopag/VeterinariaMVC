91e7e356fc82693bd041e8393b86ffe0215944c7
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-06-19 17:23:57 -0300

    Primeiro Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace atividade_iii.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
